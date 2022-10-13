8774c819ea74654a03e7d49985ced5341181f274
imjoelrios <joelrios0308@gmail.com>
2022-10-13 12:43:35 -0400

    add menu options/pages & init request functions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,295 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+// ==== UI Components ====
+// import { Home } from "./pages";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+// ==== Backend Components ====
+const axios = require("axios").default;
 
-function App() {
+// =========================================
+// ============= App =======================
+// =========================================
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container maxWidth="lg">
+      <Typography variant="h2" gutterBottom style={{ marginTop: "2%" }}>
+        Quantum Visualization
+      </Typography>
+
+      <Stack direction="row" spacing={2} style={{ marginBottom: "2%" }}>
+        <Link to="/tunneling" style={{ textDecoration: "none" }}>
+          <Button variant="outlined">Tunneling</Button>
+        </Link>
+        <Link to="/interference" style={{ textDecoration: "none" }}>
+          <Button variant="outlined">Interference</Button>
+        </Link>
+        <Link to="/spin" style={{ textDecoration: "none" }}>
+          <Button variant="outlined">Spin</Button>
+        </Link>
+        <Link to="/wave-function" style={{ textDecoration: "none" }}>
+          <Button variant="outlined">Wave Function</Button>
+        </Link>
+        <Link to="/potential-barriers" style={{ textDecoration: "none" }}>
+          <Button variant="outlined">Potential Barriers/Wells</Button>
+        </Link>
+        <Link to="/composite-functionality" style={{ textDecoration: "none" }}>
+          <Button variant="outlined">Composite Functionality</Button>
+        </Link>
+      </Stack>
+
+      <Routes>
+        <Route path="/">
+          <Route index element={<Dashboard />} />
+          <Route path="*" element={<NoMatch />} />
+          <Route path="/tunneling" element={<Tunneling />} />
+          <Route path="/interference" element={<Interference />} />
+          <Route path="/spin" element={<Spin />} />
+          <Route path="/wave-function" element={<Wave_Function />} />
+          <Route path="/potential-barriers" element={<Potential_Barriers />} />
+          <Route
+            path="/composite-functionality"
+            element={<Composite_Functionality />}
+          />
+        </Route>
+      </Routes>
+    </Container>
+  );
+}
+
+// ============= POST requests =============
+
+function generate_tunneling_model(params: any) {
+  axios
+    .post("/tunneling", {
+      firstName: "Fred",
+      lastName: "Flintstone",
+    })
+    .then(function (response: any) {
+      console.log(response);
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+}
+function generate_interference_model(params: any) {
+  axios
+    .post("/interference", {
+      firstName: "Fred",
+      lastName: "Flintstone",
+    })
+    .then(function (response: any) {
+      console.log(response);
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+}
+function generate_spin_model(params: any) {
+  axios
+    .post("/spin", {
+      firstName: "Fred",
+      lastName: "Flintstone",
+    })
+    .then(function (response: any) {
+      console.log(response);
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+}
+function generate_wave_function_model(params: any) {
+  axios
+    .post("/wave-function", {
+      firstName: "Fred",
+      lastName: "Flintstone",
+    })
+    .then(function (response: any) {
+      console.log(response);
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+}
+function generate_potential_barriers_model(params: any) {
+  axios
+    .post("/potential-barriers", {
+      firstName: "Fred",
+      lastName: "Flintstone",
+    })
+    .then(function (response: any) {
+      console.log(response);
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+}
+
+// ============= PAGES =============
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
     </div>
   );
 }
 
-export default App;
+function Dashboard() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Home
+      </Typography>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
+function Tunneling() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Tunneling
+      </Typography>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <Stack spacing={2}>
+              <TextField
+                multiline
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+              />
+              <TextField
+                multiline
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+              />
+              <TextField
+                multiline
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+              />
+              <TextField
+                multiline
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+              />
+              <Button
+                variant="contained"
+                onClick={() => {
+                  alert("clicked");
+                }}
+              >
+                Generate Model
+              </Button>
+            </Stack>
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box
+            sx={{
+              width: 300,
+              height: 300,
+              backgroundColor: "text.disabled",
+              "&:hover": {
+                backgroundColor: "text.secondary",
+                opacity: [0.9, 0.8, 0.7],
+              },
+              borderRadius: 2,
+            }}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+function Interference() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Interference
+      </Typography>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
+function Spin() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Spin
+      </Typography>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
+function Wave_Function() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Wave Function
+      </Typography>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
+function Potential_Barriers() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Potential Barriers/Wells
+      </Typography>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
+function Composite_Functionality() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Composite Functionality
+      </Typography>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
