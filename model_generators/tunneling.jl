06495700e588eccce5efe9434e8e102941b853fb
Raiden <will.rai03@gmail.com>
2022-11-06 15:48:13 -0500

    rename tunneling script & remove errors

--- model_generators/tunneling.jl
+++ model_generators/tunneling.jl
@@ -1,30 +1,35 @@
-# Tunneling and Interference
-# Computational Nano Lab, UFL
+#Tunneling 2D + 3D
+
 using Pkg
 Pkg.add("QuantumOptics")
-using QuantumOptics
 Pkg.add("LinearAlgebra")
-using LinearAlgebra
 Pkg.add("Plots")
-using Plots
 Pkg.add("ProgressMeter")
-using ProgressMeter
 Pkg.add("Images")
+Pkg.add("Colors")
+using QuantumOptics
+using LinearAlgebra
+using Plots
+using ProgressMeter
 using Images
 using ImageCore
-Pkg.add("Colors")
 using Colors
 
-# Pkg.add("pyplot")
+#Pkg.add("pyplot")
 # Had some issue, so make sure they are installed
-# Pkg.add("LaTeXStrings")
-# Pkg.add("PyCall")
+#Pkg.add("LaTeXStrings")
+#Pkg.add("PyCall")
 gr() # or use pyplot
+
+# INPUT VARIABLES
+V_tunnel = 1 #intensity of barrier (1-5)
+BarrierThickness = 3 #thickness of barrier (not the same as intensity but related) (1-5)
+Momentum = 5 #intensity of wave (1-10)
 
 # Values for initial guassian
 x0 = -5
 y0 = 0
-p0_x = 3.0
+p0_x = Momentum
 p0_y = 0.0
 σ = 2.0
 
@@ -32,34 +37,16 @@ p0_y = 0.0
 xres = 100
 yres = 80
 
-# XY field
+# XY field for 3D
 xmin = -30
 xmax = 50
 ymin = -20
 ymax = 20
 
 
-## =================== START POTENTIALS =================== ##
-# Simple potential function for a double-slit
-function potential_slit(x,y)
-    # Create the slits
-    if x > 20 && x < 25 && abs(y) > 1 && abs(y) < 5
-        return 0
-        # Fill the rest of the wall
-    elseif x > 20 && x < 25
-        return 150
-        # Make a wall at the end of the screen
-    elseif x > 48
-        return 150
-    else
-        return 0
-    end
-end
-
-V_tunnel = 4
 function potential_tunneling(x,y)
     # Fill the rest of the wall
-    if x > 12 && x < 13
+    if x > 13 - (BarrierThickness/2) && x < 13 + (BarrierThickness/2)
         return V_tunnel
     else
         return 0
@@ -78,8 +65,8 @@ b_positiony = PositionBasis(ymin, ymax, yres)
 b_momentumy = MomentumBasis(b_positiony)
 
 # Combine the basis via tensor product
-b_comp_x = b_position ⊗ b_positiony
-b_comp_p = b_momentum ⊗ b_momentumy
+b_comp_x = QuantumOpticsBase.tensor(b_position, b_positiony)
+b_comp_p = QuantumOpticsBase.tensor(b_momentum, b_momentumy)
 
 
 # Transformation between position and momentum basis
@@ -96,30 +83,27 @@ Hkiny = LazyTensor(b_comp_p, [1, 2], [one(b_momentum), py^2/2])
 Hkinx_FFT = LazyProduct(Txp, Hkinx, Tpx)
 Hkiny_FFT = LazyProduct(Txp, Hkiny, Tpx);
 
-# show gif
-function show_animated_gif(density::Array, V_plot::Array, xsample, ysample, T)
-    p = plot(show=true)
+
+    V = potentialoperator(b_comp_x, potential_tunneling)
+
+    # Finally create our hamiltonian for 3D
+    H = LazySum(Hkinx_FFT, Hkiny_FFT, V)
+
+    ψx = gaussianstate(b_position, x0, p0_x, σ)
+    ψy = gaussianstate(b_positiony, y0, p0_y, σ)
+    ψ = QuantumOpticsBase.tensor(ψx, ψy)
+
+
+    T = collect(0.0:0.1:15.0)
+    tout, ψt = timeevolution.schroedinger(T, ψ, H);
+
+    density = [Array(transpose(reshape((ψ.data), (xres, yres)))) for ψ=ψt]
+    V_plot = Array(transpose(reshape(real.(diag(V.data)), (xres, yres))))
+
+    xsample, ysample = samplepoints(b_position), samplepoints(b_positiony)
+
     prog = Progress(size(T)[1], 1)
-    @gif for t=1:size(T)[1]
-        angl = angle.(density[t])
-        mag = abs2.(density[t])
-
-
-        img = colorview(HSV, (angl.+3.14/2) * 180/pi, ones(size(mag)), mag .* (1/ maximum(mag)))
-        V_img = colorview(HSV, zeros(size(V_plot)), zeros(size(V_plot)), V_plot .*(1/maximum(V_plot)))
-        img = convert.(RGB, img)
-        V_img = convert.(RGB, V_img)
-        img = scaleminmax(RGB,0.0,1.0).(img + V_img)
-        plot!(img)
-        next!(prog)
-    end every 2
-
-end
-
-# a little better for animation
-function save_as_gif(density::Array, V_plot::Array, xsample, ysample, T)
-    prog = Progress(size(T)[1], 1)
-    anim = @animate for t=1:size(T)[1]
+    anim1 = @animate for t=1:size(T)[1]
         angl = angle.(density[t])
         mag = abs2.(density[t])
 
@@ -132,35 +116,24 @@ function save_as_gif(density::Array, V_plot::Array, xsample, ysample, T)
         plot(img)
         next!(prog)
     end
-    gif(anim, "out.gif", fps=30)
 
-end
+    # 2D Section, many variables reused
 
-function run(potential::Function, do_display::Bool) 
-    V = potentialoperator(b_comp_x, potential)
+    Txp2D = transform(b_position, b_momentum)
+    Tpx2D = transform(b_momentum, b_position)
+    H2D = LazyProduct(Txp2D, momentum(b_momentum)^2/2, Tpx2D) 
+    
+    
+    ψ2D = gaussianstate(b_position, x0, p0_x, σ)
+    
+    tout, ψt2D = timeevolution.schroedinger(T, ψ2D, H2D);
 
-    # Finally create our hamiltonian
-    H = LazySum(Hkinx_FFT, Hkiny_FFT, V)
+    anim2 = @animate for t=1:size(T)[1]
 
-    ψx = gaussianstate(b_position, x0, p0_x, σ)
-    ψy = gaussianstate(b_positiony, y0, p0_y, σ)
-    ψ = ψx ⊗ ψy
+        ψ = ψt2D[t]
+        n = abs.(ψ.data).^2
+        plot(xsample, n, xlims = (0, 120), ylims = (0, .3), title = "Probability Density |ψ(x,t)|^2", xlabel = "Position (x)", ylabel = "Probability Density (|ψ(x,t)|^2)", label = "|ψ|^2", alpha=0.3, linewidth = 2)
+    end 
 
-    T = collect(0.0:0.1:18.0)
-    tout, ψt = timeevolution.schroedinger(T, ψ, H);
-
-    density = [Array(transpose(reshape((ψ.data), (xres, yres)))) for ψ=ψt]
-    V_plot = Array(transpose(reshape(real.(diag(V.data)), (xres, yres))))
-
-    xsample, ysample = samplepoints(b_position), samplepoints(b_positiony)
-
-    # either show gif or show nothing and save as gif
-    if do_display
-        show_animated_gif(density, V_plot, xsample, ysample, T)
-    else
-        save_as_gif(density, V_plot, xsample, ysample, T)
-    end
-
-    return "Success!"
-
-end
+    gif(anim1, "3DTunneling.gif", fps=60)
+    gif(anim2, "2DTunneling.gif", fps=60)
