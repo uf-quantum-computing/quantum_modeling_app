f98539176fb5a5af527f31e0e38291e3c6a5424a
Raiden <will.rai03@gmail.com>
2023-10-11 18:28:50 -0400

    Updating app.py to accept float values

--- quantum_app_backend/app.py
+++ quantum_app_backend/app.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify, request
 from flask_restx import Api
 from flask_cors import CORS
 from watchdog.events import FileSystemEvent
+from model_generators.tunneling import Wave_Packet, Wave_Packet3D, Animator, Animator3D
+import matplotlib.pyplot as plt
+import time
 
 
 #set swagger info
@@ -15,34 +18,38 @@ api: Api = Api(
 app = Flask(__name__)
 
 api.init_app(app)
+api.init_app(app)
 
 # CORS added
-CORS(app, supports_credentials=True)
+CORS(app)
 
-@app.route('/receive_data', methods=['POST'])
-def receive_data():
-    data = request.get_json()
-    barrier = data['barrier']
-    thickness = data['thickness']
-    wave = data['wave']
-    # now we want to pass this into the tunneling.py function
-    # and return the data to the frontend
-    return jsonify({'message': 'Data received'})
+@app.route('/recieve_data/tunneling/<float:barrier>/<float:thickness>/<float:momentum>', methods=['GET'])
+def recieve_data(barrier, thickness, momentum):
+    # can pass variables through the route url /tunneling/intensity/thickness/momentum
 
-@app.route('/', methods=['GET', 'POST'])
+    start_time = time.time()  # Record the start time
+    wave_packet = Wave_Packet(n_points=500, dt=0.5, barrier_width=thickness, barrier_height=barrier, k0=momentum)
+    wave_packet3D = Wave_Packet3D(x_n_points=100, y_n_points=80, dt=0.5, BarrierThickness=thickness, barrier_height=barrier, k0=momentum)
+    animator = Animator(wave_packet)
+    animator.animate()
+    animator3D = Animator3D(wave_packet3D)
+    animator3D.animate()
+    end_time = time.time()    # Record the end time
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time} seconds")
+    plt.show()
+    plt.close()
+    return f"Created tunneling_2D.gif and tunneling_3D.gif"
+    
+
+@app.route('/hello', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
 
-    
 
 #blind namespace to swagger api page
 if __name__ == '__main__':
     app.debug = True
-
-    # from hello import api as ns1
-
-    # #bind apis
-    # api.add_namespace(ns1)
 
     #run backend server at port 5001
     app.run(host="0.0.0.0", port=3001, threaded=False, debug=True)
