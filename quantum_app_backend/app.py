9eb4dc90a8704147dc7b940f3688d84996ce10d3
Vy Vu <55815025+vudangbaovy@users.noreply.github.com>
2023-11-29 23:53:44 -0500

    added firebase dependencies, .env, fixing commit history

--- quantum_app_backend/app.py
+++ quantum_app_backend/app.py
@@ -28,21 +28,21 @@ CORS(app)
 # def default():
 #     return {}
 
-@app.route('/receive_data/tunneling/<int:barrier>/<int:thickness>/<int:momentum>', methods=['GET'])
-def receive_data(barrier, thickness, momentum):
+@app.route('/receive_data/tunneling/<int:barrier>/<int:width>/<int:momentum>', methods=['GET'])
+def receive_data(barrier, width, momentum):
     # can pass variables through the route url /tunneling/intensity/thickness/momentum
     print("You evoked the API successfully")
     plt.switch_backend('Agg') 
     
     start_2d_time = time.time()  # Record the start time
-    wave_packet = Wave_Packet(n_points=500, dt=0.75, barrier_width=thickness, barrier_height=barrier, k0=momentum)
+    wave_packet = Wave_Packet(n_points=500, dt=0.5, barrier_width=width, barrier_height=barrier, k0=momentum)
     animator = Animator2D(wave_packet)
     base64Gif2D = animator.animate2D()
     end_2d_time = time.time()    # Record the end time
     elapsed_2d_time = end_2d_time - start_2d_time
     print(f"Elapsed 2D generator time: {elapsed_2d_time} seconds")
     start_3d_time = time.time()  # Record the start time
-    wave_packet3D = Wave_Packet3D(x_n_points=100, y_n_points=80, dt=0.75, BarrierThickness=thickness, barrier_height=barrier, k0=momentum)
+    wave_packet3D = Wave_Packet3D(x_n_points=500, y_n_points=400, dt=0.5, barrier_width=width, barrier_height=barrier, k0=momentum)
     animator3D = Animator3D(wave_packet3D)
     base64Gif3D = animator3D.animate3D()
     end_3d_time = time.time()    # Record the end time
--- src/pages/Tunneling.tsx
+++ src/pages/Tunneling.tsx
@@ -280,7 +280,7 @@ const Tunneling = () => {
         </Sider>
 
         {/* ======================== Content ======================== */}
-        <Layout style={{ margin: "5%" }}>
+        {/* <Layout style={{ margin: "5%" }}>
           <Content>
           <Grid container spacing={3}>
           <Grid item xs={12} sm={6}>
@@ -295,6 +295,27 @@ const Tunneling = () => {
             </Grid>
           </Grid>
 
+          </Content>
+        </Layout> */}
+        {/* ======================== Content ======================== */}
+        <Layout style={{ margin: "5%" }}>
+          <Content>
+            <CustomPageHeader text="Tunneling" size="h3" />
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+              <img src={`data:image/video;base64,${tunneling_img3d_base64}`} alt="3D Image" style={{ width: "100%" }} />
+              <Typography variant="body1" align="center" style={{marginTop: '5px'}}>3D Tunneling Model</Typography>
+              </Grid>
+              <Grid item xs={6}>
+              <img src={`data:image/video;base64,${tunneling_img2d_base64}`} alt="2D Image" style={{ width: "100%" }} />
+              <Typography variant="body1" align="center" style={{marginTop: '5px'}}>2D Tunneling Model</Typography>
+              </Grid>
+            </Grid>
+            <CustomDescriptionBox
+              msg={
+                "Quantum tunneling, also known as tunneling is a quantum mechanical phenomenon whereby a wavefunction can propagate through a potential barrier. The transmission through the barrier can be finite and depends exponentially on the barrier height and barrier width.The non-unitary probability density (previously described in wave functions) of elementary particles causes some expected behaviors observed in classical physics to not fully translate into quantum physics. Elementary particles behave with a degree of unpredictability, with their position being partially decoupled from their “expected” position relative to their probability density and thereby their wave function. As a result, such particles can “tunnel” through potential energy barriers. Imagine throwing a tennis ball at a wall. In classical physics, the tennis ball will bounce off the wall every time. In quantum physics, sometimes your tennis ball will slip part way through the wall before bouncing back and, if the wall is thin and weak enough, it will travel through the wall at a reduced momentum. The ball will also frequently reflect off the wall as expected with the probability of tunneling through the wall being increased if the barrier is physically thinner and if it is weaker, in quantum physics a weaker barrier would have a lower magnitude of potential energy. A particle, or ball, traveling with higher kinetic energy also tends to tunnel more often. Try changing the width and intensity of the barrier as well as the energy of the particle. Does the particle always tunnel? What shape does the probability density function take once it meets the wall? How does it relate to the shape of the probability function as it is propagating?"
+              }
+            />
           </Content>
         </Layout>
       </Layout>
