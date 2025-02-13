dd4602f0a2863b32d2a69f4bae4e72a1fbc39fca
Vy Vu <55815025+vudangbaovy@users.noreply.github.com>
2025-02-12 23:26:07 -0500

    Downsized and uploaded interference model, abstracted db API, relative imports

--- quantum_app_backend/db.py
+++ quantum_app_backend/db.py
@@ -14,45 +14,33 @@ class MongoConnector:
     def __init__(self):
         self.client = pymongo.MongoClient(config['MONGO']['MONGO_URI'])
         self.db = self.client['models']
+        self.model = ''
 
     def set_collection(self, model):
         self.collection = self.db[model]
+        self.model = model
 
     def close(self):
+        self.model = ''
         self.client.close()
 
     def get(self, parameters):
-        for key, value in parameters.items():
-            if key not in VARIABLES:
-                raise ValueError(f'{key} is not a valid model')
-            if value not in VARIABLES[key]:
-                raise ValueError(f'{value} is not a valid parameter for model {key}')
+        for param, value in parameters.items():
+            if param not in VARIABLES[self.model]:
+                raise ValueError(f'{param} is not a valid parameter for model {self.model}')
+            if not isinstance(value, (int, float)):
+                raise ValueError(f'{value} is not a valid value for parameter {param}')
         
         try:
-            model = self.collection.find({'parameters': parameters})
-            if model:
-                return model.sort('uploadDate', pymongo.DESCENDING).limit(1)[0].decode('utf-8')
+            res = self.collection.find({'parameters': parameters})
+            if res:
+                document = res.sort('uploadDate', pymongo.DESCENDING).limit(1)[0]
+                return document['animation'].decode('utf-8')
+            else:
+                return None
         except IndexError as e:
             print(e)
             return None
-
-    # def get_tunneling(self, barrier, width, momentum):
-    #     try:
-    #         model = self.tunneling_collection.find({'filename':f'probs_{momentum}_{barrier}_{width}_3D.html'})
-    #         if model:
-    #             return model.sort('uploadDate', pymongo.DESCENDING).limit(1)[0]
-    #     except IndexError as e:
-    #         print(e)
-    #         return None
-
-    # def get_interference(self, momentum, spacing, slit_separation):
-    #     try:
-    #         model = self.interference_collection.find({'filename':f'probs_{momentum}_{spacing}_{slit_separation}_3D.html'})
-    #         if model:
-    #             return model.sort('uploadDate', pymongo.DESCENDING).limit(1)[0]
-    #     except IndexError as e:
-    #         print(e)
-    #         return None
         
     def upload_model(self, parameters, animation):
         try:
