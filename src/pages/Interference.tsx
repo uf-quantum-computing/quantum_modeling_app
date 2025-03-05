91365e3ad31931db8d93441ea584f6531982b544
Raiden <will.rai03@gmail.com>
2025-03-05 08:46:44 -0500

    Changing the scaling of animations on tunneling and interference pages

--- src/pages/Interference.tsx
+++ src/pages/Interference.tsx
@@ -199,7 +199,7 @@ const Interference = () => {
     const divHeight = animationDivRef.current.offsetHeight || 400; 
 
     // Calculate scale based on both width and height constraints
-    const widthScale = (flexWidth - 300) / 1200;
+    const widthScale = (flexWidth) / 1200;
     const heightScale = flexHeight / 700; // Assuming 700 is the original height of animation
 
     const newScale = Math.min(widthScale, heightScale); // Ensure it doesn't exceed the box height
