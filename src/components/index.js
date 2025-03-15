03b8ec25146d8c0e7ac6120cfeb6e46db89b8447
Raiden <will.rai03@gmail.com>
2025-03-15 01:25:47 -0400

    Making a custom component for the sliders

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as CustomPageHeader } from "./CustomPageHeader.tsx";
 export { default as CustomTitle } from "./CustomTitle.tsx";
 export { default as Dashboard } from "./Dashboard.tsx";
 export { default as Navbar } from "./Navbar.tsx";
+export { default as CustomSlider } from "./CustomSlider.tsx";
