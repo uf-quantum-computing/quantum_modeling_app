69ff89e461edd522d8148ade014e6fb4fbd4a26c
Vy Vu <55815025+vudangbaovy@users.noreply.github.com>
2025-03-27 13:17:34 -0400

    run socketio server with eventlet instead of gunicorn server

--- quantum_app_backend/gunicorn_config.py
+++ quantum_app_backend/gunicorn_config.py
@@ -1,7 +1,5 @@
 import os
 
-
-
 workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
@@ -9,8 +7,6 @@ threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 # timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:3001')
-
-
 
 forwarded_allow_ips = '*'
 
